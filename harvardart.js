33c504ff2e25639e63d388cb27bbb9d6338d10e9
Jianwen Darryl Laiu <darryl.laiu@yale.edu>
2018-09-13 01:12:24 +0000

    Added objects + getImages

--- harvardart.js
+++ harvardart.js
@@ -1,6 +1,43 @@
 let button = document.getElementById("getGalleries");
 button.addEventListener("click", getGalleries);
 
+function getImage(url) {
+    url = url + '/full/full/0/default.jpg'
+    let image = `<img src=${url}>`
+    return image;
+}
+
+function getObjectDetails(objectId) {
+    let url = 'https://api.harvardartmuseums.org/object/' + objectId + '?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce';
+    let output = `<h2>Details</h2>` + `<table>`
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        for (let record in data) {
+            output += `
+                <tr><td>` + record + `</td><td>` + data[record] + `</td></tr>`;
+        };
+        output += `</table> `
+        //console.log(output);
+        document.getElementById('objectDetails').innerHTML = output;
+        let printImage = `<h2> Picture </h2>`;
+        let images = data.images;
+
+        images.forEach((image) => {
+            console.log(image.iiifbaseuri);
+            printImage += getImage(image.iiifbaseuri);
+        })
+
+        // for(let image in images) {
+        //     console.log(image.iiifbaseuri)
+        //     //printImage += getImage(image.iiifbaseuri);
+        // }
+        document.getElementById('image').innerHTML = printImage;
+    });
+
+}
+
 function getObjects(id) {
     let url = 'https://api.harvardartmuseums.org/object?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce&gallery=' + id;
     fetch(url)
@@ -26,12 +63,12 @@ function getObjects(id) {
                 records.forEach((object) => {
                     objects += `
                         <div>
-                            <p>${object.title}</p>
+                            <button id="${object.id}" onClick="getObjectDetails(this.id)">${object.title}</button>
                         </div>
                     `;
                 });
 
-                console.log(objects);
+                //console.log(objects);
                 document.getElementById('objects').innerHTML = objects;
             });
 
