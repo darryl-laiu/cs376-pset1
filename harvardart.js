6f525db4ea8b9a0204464d4808cd0b5bfe018d4d
Jianwen Darryl Laiu <darryl.laiu@yale.edu>
2018-09-15 00:59:19 +0000

    completed pset, added specific getDetails functions for each form and created indivdualised button

--- harvardart.js
+++ harvardart.js
@@ -1,20 +1,9 @@
+//Additional note: Discussed code with Matthias and Toby
+
 let button = document.getElementById("getGalleries");
 button.addEventListener("click", getGalleries);
 
-// let formButton = document.getElementById("getForm");
-// formButton.addEventListener("click", showForm);
-
-// function showForm() {
-//     let newForm;
-//     newForm = `<form class="contact-form">
-//                 <input type="text" name=""/>
-//                 <input type="submit" value="Submit"/>
-//             </form>`;
-
-//     console.log(newForm);
-//     document.getElementById('createForm').innerHTML = newForm;
-// }
-
+//method to expand the full image
 function fullImage(url, format) {
     //console.log(format);
     newUrl = url + '/full/full/0/default.' + format;
@@ -22,6 +11,7 @@ function fullImage(url, format) {
     document.getElementById('image').innerHTML = image;
 }
 
+//method to get all the details for an object
 async function getObjectDetails(objectId) {
     //get details
     let url = 'https://api.harvardartmuseums.org/object/' + objectId + '?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce';
@@ -65,7 +55,7 @@ async function getObjectDetails(objectId) {
     document.getElementById('image').innerHTML = printImage;
 }
 
-
+//method to get all objects within gallery
 async function getObjects(id) {
     let url = 'https://api.harvardartmuseums.org/object?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce&gallery=' + id;
 
@@ -100,6 +90,7 @@ async function getObjects(id) {
     document.getElementById('objects').innerHTML = objects;
 }
 
+//method to get list of all galleries
 async function getGalleries() {
     let response = await fetch('https://api.harvardartmuseums.org/gallery?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce')
     response = await response.json();
@@ -122,6 +113,7 @@ async function getGalleries() {
     document.getElementById('output').innerHTML = output;
 }
 
+//abstracting the methods for the form into a class, so that it can be individualised for each form type
 class manageForm {
     constructor(formId) {
         /**
@@ -159,16 +151,20 @@ class manageForm {
     }
 }
 
+//creating instances of manageForm for each type of form
 const objectForm = new manageForm("objectForm");
 const galleryForm = new manageForm("galleryForm");
 const exhibitionForm = new manageForm("exhibitionForm");
+const personForm = new manageForm("personForm")
 
-
+//method to fetch search queries
 async function search(data) {
     let parameter;
     let searchTerm;
     let type;
 
+    //to pull the relevant needed information from the form.
+    //this is assuming the user will only search within 1 field
     for (let record in data) {
         if(data[record] !== "" && record !== "type") {
             // console.log(data);
@@ -184,13 +180,14 @@ async function search(data) {
 
     console.log(type);
 
-    //note to change the construction of url if need to use 'q=field:value'
     let url = 'https://api.harvardartmuseums.org/' + type + '/?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce&size=100&q=' + parameter + ':' + searchTerm;
     console.log(url);
     let res = await fetch(url);
     res = await res.json();
     results = res.records;
     //console.log(results);
+
+    //getting all the data from all pages and and storing before displaying onto html
     let pages = res.info.pages;
     let pageNum = "&page="
     for(let i = 2; i <= pages; i++) {
@@ -201,19 +198,84 @@ async function search(data) {
         results = results.concat(newRes);
     }
 
+    //need to customise buttons for each type of form because of the way i wrote the "onClick" methods
     let output = `<h2>Search Results</h2>`
     for(let record in results) {
         //console.log(results[record]);
-        output += `
+        if(type === "object") {
+            output += `
                 <div>
                     <button id="${results[record].id}" onClick="getObjectDetails(this.id)">${results[record].title}</button>
                 </div>
             `;
+        } else if (type === "exhibition") {
+            output += `
+                <div>
+                    <button id="${results[record].id}" onClick="getExhibitionDetails(this.id)">${results[record].title}</button>
+                </div>
+            `;
+        } else if (type === "gallery") {
+            output += `
+                <div>
+                    <button id="${results[record].id}" onClick="getGalleryDetails(this.id)">${results[record].name}</button>
+                </div>
+            `;
+        } else if (type === "person") {
+            output += `
+                <div>
+                    <button id="${results[record].personid}" onClick="getPersonDetails(this.id)">${results[record].displayname}</button>
+                </div>
+            `;
+        }
     }
     document.getElementsByClassName('results__display')[0].innerHTML = output;
 
 }
 
-function getDetails(id) {
-    
+//specific method to get details when click on search result of a person
+async function getPersonDetails(id) {
+    //get details
+    let url = 'https://api.harvardartmuseums.org/person/' + id + '?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce';
+    let output = `<h2>Details</h2>` + `<table>`;
+
+    let res = await fetch(url);
+    res = await res.json();
+    for (let record in res) {
+        output += `
+            <tr><td>` + record + `</td><td>` + res[record] + `</td></tr>`;
+    };
+    output += `</table> `;
+    document.getElementById('objectDetails').innerHTML = output;
+}
+
+//specific method to get details when click on search result of gallery
+async function getGalleryDetails(id) {
+    //get details
+    let url = 'https://api.harvardartmuseums.org/gallery/' + id + '?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce';
+    let output = `<h2>Details</h2>` + `<table>`;
+
+    let res = await fetch(url);
+    res = await res.json();
+    for (let record in res) {
+        output += `
+            <tr><td>` + record + `</td><td>` + res[record] + `</td></tr>`;
+    };
+    output += `</table> `;
+    document.getElementById('objectDetails').innerHTML = output;
+}
+
+//specific method to get details when click on search result of exhibition
+async function getExhibitionDetails(id) {
+    //get details
+    let url = 'https://api.harvardartmuseums.org/exhibition/' + id + '?apikey=fc1b68f0-b251-11e8-b0af-a198bfdbf6ce';
+    let output = `<h2>Details</h2>` + `<table>`;
+
+    let res = await fetch(url);
+    res = await res.json();
+    for (let record in res) {
+        output += `
+            <tr><td>` + record + `</td><td>` + res[record] + `</td></tr>`;
+    };
+    output += `</table> `;
+    document.getElementById('objectDetails').innerHTML = output;
 }
